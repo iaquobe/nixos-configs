6aad621ec4eb6508f4ed927390193a18ebe48062
Jakob Lambert-Hartmann <lamberth@in.tum.de>
2023-10-23 20:32:23 +0200

    install script nvchad and tmux

--- setup.sh
+++ setup.sh
@@ -1,19 +1,22 @@
 SCRIPTPATH=$(dirname "$(realpath "$0")")
 sudo rm /etc/nixos
-rm ~/.config/nvim
-rm ~/.config/tmux
-rm ~/.config/waybar
-rm ~/.config/hypr
-rm ~/.config/kitty
-rm ~/.config/zsh
-rm ~/.config/eww
+rm ~/.config/{nvim,tmux,waybar,hypr,kitty,zsh,eww,home-manager/home.nix}
 
 
+mkdir ~/.config/home-manager/
 sudo ln -s $SCRIPTPATH/nixos/ /etc/nixos
+sudo ln -s $SCRIPTPATH/nixos/home.nix ~/.config/home-manager/home.nix
 
-ln -s $SCRIPTPATH/config/nvim ~/.config
-ln -s $SCRIPTPATH/config/eww ~/.config
+# nvchad
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+rm -r ~/.config/nvim/lua/custom/
+ln -s $SCRIPTPATH/config/nvim/custom ~/.config/nvim/lua/custom
+
+#tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s $SCRIPTPATH/config/tmux ~/.config
+
+ln -s $SCRIPTPATH/config/eww ~/.config
 ln -s $SCRIPTPATH/config/waybar ~/.config
 ln -s $SCRIPTPATH/config/hypr ~/.config
 ln -s $SCRIPTPATH/config/kitty ~/.config
